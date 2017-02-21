fcecbd899a57d797184ab6e233a84254dd3c7456
hannes <me@hannes-lehmann.com>
2017-02-21 15:04:14 +0100

    directory layout change

--- common/common_enc.go
+++
@@ -1,9 +0,0 @@
-package common
-
-var ListenPort = "8100"
-
-var JWTSignKey = []byte("Z9HGGfvaCYuJnBXCwXOga2BRM88GVRl6KRM1QKCoIP7xkGcWoZt6AXyfk4xCe8+3")
-
-// Payload encryption
-var EncKey = []byte("vfj5DLWmS0FiQzt1J5JAJKJX8jDskrBt")
-var Nonce = []byte("Qzt1JD1l5JBt")
